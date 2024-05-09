970e45a5bc4d55050207e1c51e86d4c8f3bdd2f8
paulSusmita <162417267+paulSusmita@users.noreply.github.com>
2024-05-09 20:38:05 +0530

    Add files via upload

---
+++ currencyConverter.js
@@ -0,0 +1,43 @@
+import { LightningElement } from 'lwc';
+import {countryCodeList} from 'c/countryCodeList'
+import currencyConverterAsset from '@salesforce/resourceUrl/currencyConverterAsset'
+
+export default class CurrencyConverter extends LightningElement {
+    currencyImage = currencyConverterAsset + '/currencyConverterAssets/currency.svg'
+    countryCode = countryCodeList
+    countryFrom = "INR"
+    countryTo = "USD"
+    amount=''
+    result
+    error
+
+    handleChange(event){
+        const{name,value}= event.target
+        console.log("Name: ",name)
+        console.log("value: ",value)
+        this[name] = value
+        this.result=''
+        this.error=''
+    }
+    submitHandler(event){
+        event.preventDefault();
+        this.convert();
+    }
+    async convert(){
+        // const API_URL = `https://api.exchangerate.host/convert?access_key=${AUTH_KEY}&from=${this.countryFrom}&to=${this.countryTo}`
+        const API_KEY = '17df10dae232584771d63473'
+        const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${this.countryFrom}/${this.countryTo}`
+        try{
+          const data = await fetch(API_URL)
+          const jsonData = await data.json()
+          debugger;
+          // this.result = (Number(this.amount) * jsonData.result).toFixed(2)
+          this.result = (Number(this.amount) * jsonData.conversion_rate).toFixed(2)
+          console.log(this.result)
+        } catch(error){
+          console.log(error)
+          this.error="An error occurred. Please try again..."
+        }
+      }
+    
+}
